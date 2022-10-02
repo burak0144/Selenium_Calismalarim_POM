5fe2b9dac3b9cebc069b5192f9915eaf2033f2d6
burak <karanfilkaranfil822@gmail.com>
2022-10-02 11:45:12 +0300

    "2.10.22"

---
+++ src/test/java/pages/HotelMyCampPageWebTables.java
@@ -0,0 +1,49 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+import java.util.List;
+
+public class HotelMyCampPageWebTables {
+   public HotelMyCampPageWebTables(){
+        PageFactory.initElements(Driver.getDriver(),this);
+
+    }
+    @FindBy(xpath = "//a[text()='Log in']")
+    public WebElement ilkLoginLinki;
+
+    @FindBy(xpath = "//input[@type='text']")
+    public WebElement userNameBox;
+
+    @FindBy(xpath = "//input[@id='Password']")
+    public WebElement passwordBox;
+
+    @FindBy(xpath = "//input[@id='btnSubmit']")
+    public WebElement loginButonu;
+
+    @FindBy(xpath = "//thead//tr[1]//th")
+    public List<WebElement> headerBirinciSatirDatalar;
+
+    @FindBy(xpath = "//tbody//tr//td")
+    public List<WebElement> tumBodyDatalarList;
+
+    @FindBy(xpath = "//table")
+    public WebElement tumBodyveHeader;
+
+    @FindBy(xpath = "//tbody//tr")
+    public List<WebElement> satirlarListesi;
+
+    public void girisYap(){
+        Driver.getDriver().get(ConfigReader.getProperty("HMCUrl"));
+        ilkLoginLinki.click();
+        userNameBox.sendKeys(ConfigReader.getProperty("HMCValidUsername"));
+        passwordBox.sendKeys(ConfigReader.getProperty("HMCValidPassword"));
+        loginButonu.click();
+    }
+
+}
+
--- src/test/java/tests/day18/C01_WebTables.java
+++ src/test/java/tests/day18/C01_WebTables.java
@@ -1,4 +1,82 @@
 package tests.day18;
 
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import pages.HotelMyCampPage;
+import pages.HotelMyCampPageWebTables;
+
+import java.util.List;
+
 public class C01_WebTables {
+    HotelMyCampPageWebTables hotelMyCampPageWebTables;
+
+    @Test
+    public void loginT() {
+
+        //● login( ) metodun oluşturun ve oturum açın.
+        //● https://www.hotelmycamp.com admin/HotelRoomAdmin adresine gidin
+        //○ Username : manager
+        //○ Password : Manager1!
+        hotelMyCampPageWebTables= new HotelMyCampPageWebTables();
+        hotelMyCampPageWebTables.girisYap();
+    }
+
+    @Test(dependsOnMethods ="loginT" )
+    public void table() {
+
+    //● table( ) metodu oluşturun
+    //○ Tüm table body’sinin boyutunu(sutun sayisi) bulun. //thead//tr[1]//th
+        System.out.println("============header yani sutun sayisi");
+      List<WebElement> headerDataList=hotelMyCampPageWebTables.headerBirinciSatirDatalar;
+        System.out.println("tablodaki sutun sayisi  = " + headerDataList.size());
+
+        // header kisminda birinci satir ve altindaki datalari locate edelim
+    //      //thead//tr[1]//th"
+        System.out.println("============Header kismindaki 1. satir");
+        for (WebElement w:headerDataList
+             ) {
+            System.out.println(w.getText());
+        }
+
+    //○ Table’daki tum body’I ve başlıkları(headers) konsolda yazdırın.
+    // //table basliklarla birlikte yazdirir  yada   //tbody basliksiz butun table yazar
+        System.out.println("==========tum datalar tek String halinde");
+       WebElement tumBody=hotelMyCampPageWebTables.tumBodyveHeader;
+        System.out.println(tumBody.getText());
+    // eger body'i tek bir webelement olarak locate edersek
+    // icindeki tum datalari gettext() ile yazdirabiliriz
+    // ancak bu durumda bu elementler ayri ayri degil body'nin icindeki tek bir String'in parcalari olurlar
+    // dolayisiyla bu elementlere tek tek ulasmamiz mumkun olmaz
+    // sadece contains methodu ile body'de olup olmadiklarini test ederiz
+
+    // eger her datayi ayri ayri almak istersek
+    ////tbody//tr//td yada //tbody//td   sekline locate edip bir list'e atabiliriz.
+        System.out.println("==========tum datalar ayri ayri");
+        List<WebElement>tumBodyList=hotelMyCampPageWebTables.tumBodyDatalarList;
+        tumBodyList.forEach(t-> System.out.println(t.getText()));
+}
+
+    @Test(dependsOnMethods = "loginT")
+    public void printRows() {
+
+    //● printRows( ) metodu oluşturun //tr
+
+    //○ table body’sinde bulunan toplam satir(row) sayısını bulun.
+        //sayet baslik larla alacak olsaydik //tr olurdu ama body'de dedigi icin //tbody//tr
+        System.out.println("==============satirlarin boyutu");
+      List<WebElement>satirlarList=hotelMyCampPageWebTables.satirlarListesi;
+        System.out.println("satirlarListesinin Boyutu = " + satirlarList.size());
+        //listeyi burayada alabiliriz almadan da yazdirilir
+
+        System.out.println("============page sayfasindan direkt yazdirilir");
+        System.out.println(hotelMyCampPageWebTables.satirlarListesi.size());
+        //○ Table body’sinde bulunan satirlari(rows) konsolda yazdırın.
+        System.out.println("================satirlar listesi");
+        satirlarList.forEach(t-> System.out.println(t.getText()));
+
+    //○ 4.satirdaki(row) elementleri konsolda yazdırın
+        System.out.println("============4.satir");
+        System.out.println("4.satir = " + satirlarList.get(3).getText());
+        //index 0 dan basladigindan dolayi 4.satir 3.index'te olur
+    }
 }
