a1d1f4342009b61e5bd470bd30660a5e0b960350
burak <karanfilkaranfil822@gmail.com>
2022-10-04 00:14:03 +0300

    "2.10.22"

--- src/test/java/tests/day20/C03_ReusableRunner.java
+++ src/test/java/tests/day20/C03_ReusableRunner.java
@@ -1,5 +1,19 @@
 package tests.day20;
 
-public class C03_ReusableRunner {
+import org.apache.poi.ss.usermodel.Cell;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.ReusableMethods;
 
+public class C03_ReusableRunner {
+    @Test
+    public void test01() {
+        // Ulkeler excel'indeki Sayfa1'de
+        // 11.index deki satirin 2.index deki hucresinin Azerbaycan oldugunu test edin.
+
+        String path="src/test/java/resources/ulkeler.xlsx";
+        String expectedData="Azerbaycan";
+        Cell cell = ReusableMethods.hucreGetir(path,"Sayfa1",11,2);
+        Assert.assertEquals(cell.toString(), expectedData);
+    }
 }
--- src/test/java/tests/day20/C05_WriteExcel.java
+++ src/test/java/tests/day20/C05_WriteExcel.java
@@ -1,14 +1,47 @@
 package tests.day20;
 
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class C05_WriteExcel {
     //1) Yeni bir Class olusturalim WriteExcel
+    Workbook workbook;
+    @Test
+    public void writeExcelTest() throws IOException {
+
     //2) Yeni bir test method olusturalim writeExcelTest()
-    //3) Adimlari takip ederek 1.satira kadar gidelim
-    //4) 4.hucreye yeni bir cell olusturalim
-    //5) Olusturdugumuz hucreye “Nufus” yazdiralim
-    //6) 2.satir nufus kolonuna 1500000 yazdiralim
-    //7) 10.satir nufus kolonuna 250000 yazdiralim
-    //8) 15.satir nufus kolonuna 54000 yazdiralim
+
+        String path="src/test/java/resources/ulkeler.xlsx";
+
+        try {
+            FileInputStream fis=new FileInputStream(path); //okuyacagiz iceri aliyoruz
+             workbook= WorkbookFactory.create(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //3) Adimlari takip ederek 1.satira kadar gidelim
+        //4) 5.hucreye yeni bir cell olusturalim
+        //5) Olusturdugumuz hucreye “NUMARA” yazdiralim
+        workbook.getSheet("Sayfa1").getRow(0).createCell(5).setCellValue("NUMARA");
+
+    //6) 2.satir NUMARA kolonuna 4634234 yazdiralim
+        workbook.getSheet("Sayfa1").getRow(1).createCell(5).setCellValue("4634234");
+    //7) 10.satir NUMARA kolonuna 10923634 yazdiralim
+        workbook.getSheet("Sayfa1").getRow(10).createCell(5).setCellValue("10923634");
+    //8) 15.satir NUMARA kolonuna 54022131 yazdiralim
+        workbook.getSheet("Sayfa1").getRow(10).createCell(5).setCellValue("54022131");
     //9) Dosyayi kaydedelim
+        FileOutputStream fos=new FileOutputStream(path); //yazdiriyoruz disari gonderiyoruz
+       workbook.write(fos);
     //10)Dosyayi kapatalim
+
+        //FileOutputStrem Write yazar disari gonderir
+        //FileInputStrem Read okur iceri alir
+}
 }
