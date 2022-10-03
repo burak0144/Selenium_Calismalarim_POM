6045e7531de698ac1700d62b7cc46c672926a93a
burak <karanfilkaranfil822@gmail.com>
2022-10-03 13:41:23 +0300

    "2.10.22"

--- src/test/java/tests/day20/C01_ReadExcel.java
+++ src/test/java/tests/day20/C01_ReadExcel.java
@@ -1,17 +1,54 @@
 package tests.day20;
 
+import org.apache.poi.ss.usermodel.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class C01_ReadExcel {
     //1. apache poi dependency’i pom file’a ekleyelim
     //2. Java klasoru altinda resources klasoru olusturalim
     //3. Excel dosyamizi resources klasorune ekleyelim
     //4. excelAutomation isminde bir package olusturalim
     //5. ReadExcel isminde bir class olusturalim
+
+    @Test
+    public void readExcel() throws IOException {
     //6. readExcel() method olusturalim
     //7. Dosya yolunu bir String degiskene atayalim
+        String path="src/test/java/resources/ulkeler.xlsx";
     //8. FileInputStream objesi olusturup,parametre olarak dosya yolunu girelim
+        FileInputStream fis=new FileInputStream(path);
     //9. Workbook objesi olusturalim,parameter olarak fileInputStream objesini girelim
-    //10. WorkbookFactory.create(fileInputStream)
+        //10. WorkbookFactory.create(fileInputStream)
+        Workbook workbook= WorkbookFactory.create(fis);
     //11. Worksheet objesi olusturun workbook.getSheetAt(index)
+        Sheet sheet=workbook.getSheet("Sayfa1");
     //12. Row objesi olusturun sheet.getRow(index)
-    //13. Cell objesi olusturun row.getCell(index
+        Row row=sheet.getRow(4);
+    //13. Cell objesi olusturun row.getCell(index)
+        Cell cell=row.getCell(2);
+
+        System.out.println("Sayfa 1 deki 4.indexteki satir ve 2. index'teki sutunda bulunan data :"+cell);
+
+        // biz FIS ile okudugumuz excel dosyasini projemiz icerisinde kullanabilmek icin
+        // Apachi POI dependency yardimi ile bir Workbook olusturduk
+
+        // Bu workbook bizim projemizin icerisinde ulkeler excelinin bir kopyasini kullanmamizi sagliyor
+
+        // Excel'in yapisis geregi bir hucreye ulasabilmek icin workbook dan baslayarak
+        // sirasiyla sheet, row ve cell objeleri olusturmamiz gerekir
+
+        //indexi4 olan satirdaki , indexi 2 olan hucrenin Andorra oldugunu test edin
+        Assert.assertEquals(cell.toString(),"Andorra");
+        //data turu cell oldugu icin .toString methodunu kullanmaliyiz
+
+        // 5. indexteki satirin 3.indexdeki hucre bilgisini yazdiralim
+        row=sheet.getRow(5);
+        cell=row.getCell(3);
+        System.out.println("Sayfa 1 deki 5.indexteki satir ve 3. index'teki sutunda bulunan data :"+cell);
+}
 }
--- src/test/java/tests/day20/C02_ReadExcel.java
+++ src/test/java/tests/day20/C02_ReadExcel.java
@@ -1,12 +1,36 @@
 package tests.day20;
 
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class C02_ReadExcel {
+    @Test
+    public void readExcel2() throws IOException {
+        SoftAssert softAssert = new SoftAssert();
+
     //Yeni bir test method olusturalim readExcel2( )
     //- 1.satirdaki 2.hucreye gidelim ve yazdiralim
+    String path="src/test/java/resources/ulkeler.xlsx";
     //- 1.satirdaki 2.hucreyi bir string degiskene atayalim ve yazdiralim
+    FileInputStream fis=new FileInputStream(path);
+    Workbook workbook= WorkbookFactory.create(fis);
+       String cell= workbook.getSheet("Sayfa1").getRow(0).getCell(1).toString();
+        System.out.println(cell);
     //- 2.satir 4.cell’in afganistan’in baskenti oldugunu test edelim
+        String expectedData="Kabil";
+        String cell2=workbook.getSheet("Sayfa1").getRow(1).getCell(3).toString();
+        softAssert.assertEquals(cell2,expectedData);
     //- Satir sayisini bulalim
+
     //- Fiziki olarak kullanilan satir sayisini bulun
     //- Ingilizce Ulke isimleri ve baskentleri bir map olarak kaydedelim
-
+       softAssert.assertAll();
+}
 }
